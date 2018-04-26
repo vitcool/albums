fe0d8baffa440a1ea83ec6c6ad72155bcfb2eae0
Vitalii Kulyk <vitcool@ukr.net>
2018-04-26 08:40:59 +0300

    lection 31: Header props is added + album list component is created

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // import library
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create component
 const App = () => (
-    <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 //render compoent to device
--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,27 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 //create compoennt
-const Header = () => {
+const Header = ({ headerText }) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
