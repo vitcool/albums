49f7b11a779c90ca7c7ecfb98129f05b5e2c66b5
Vitalii Kulyk <vitcool@ukr.net>
2018-04-29 13:46:35 +0300

    lection 36 - class based album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album list</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(responce => this.setState({ albums: responce.data }));
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        <Text>Album list</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
